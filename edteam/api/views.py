b9298b972ac75add997fdb3e5d71bdc5088d0662
DaniloTrocel <158530079+DaniloTrocel@users.noreply.github.com>
2024-06-24 22:44:18 -0400

    Creacion de APIs con solo Django

---
+++ edteam/api/views.py
@@ -0,0 +1,53 @@
+from django.http import JsonResponse
+
+from .models import Curso
+
+import json
+
+from django.views.decorators.csrf import csrf_exempt
+
+def index(request):
+    context ={
+        'status': True,
+        'content': 'mi primer api rest con django'
+    }
+
+    return JsonResponse(context)
+
+def curso(request):
+    listado_cursos = Curso.objects.all()
+
+    context = {
+        'status': True,
+        'content': list(listado_cursos.values())
+    }
+
+    return JsonResponse(context)
+
+@csrf_exempt
+def post_curso(request):
+    json_data = json.loads(request.body)
+
+    titulo = json_data['titulo']
+    descripcion = json_data['descripcion']
+    imagen = json_data['imagen']
+
+    nuevo_curso = Curso.objects.create(
+        titulo = titulo,
+        descripcion = descripcion,
+        imagen = imagen
+    )
+
+    dic_nuevo_curso = {
+        'id': nuevo_curso.id,
+        'titulo': nuevo_curso.titulo,
+        'descripcion': nuevo_curso.descripcion,
+        'imagen': nuevo_curso.imagen
+    }
+
+    context = {
+        'status': True,
+        'content': dic_nuevo_curso
+    }
+
+    return JsonResponse(context)
